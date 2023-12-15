9dfa20171bf1ecc8c6b309c7949f8b9fd9c123a2
ayushsonani <sonaniayush5@gmail.com>
2023-12-15 10:39:00 +0530

    second update

--- lib/api_page.dart
+++ lib/api_page.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 
 class HomeScreen extends StatefulWidget {
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -24,25 +23,19 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white.withOpacity(0.94),
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             PieChart(
-              dataMap: {
-                "vayaj": 350000,
-                "bank": 1000000,
-                "rokada": 1600000
-              },
-              chartRadius: MediaQuery
-                  .of(context)
-                  .size
-                  .width / 3.2,
+              dataMap: {"vayaj": 350000, "bank": 1000000, "rokada": 1600000},
+              chartRadius: MediaQuery.of(context).size.width / 3.2,
               legendOptions: LegendOptions(legendPosition: LegendPosition.left),
               chartType: ChartType.ring,
-              chartValuesOptions: ChartValuesOptions(
-                  showChartValuesInPercentage: true),
+              chartValuesOptions:
+                  ChartValuesOptions(showChartValuesInPercentage: true),
               animationDuration: Duration(seconds: 2),
               colorList: [
                 Colors.red,
@@ -50,10 +43,9 @@ class _HomeScreenState extends State<HomeScreen> {
                 Colors.green,
               ],
             ),
-            SizedBox(height: MediaQuery
-                .of(context)
-                .size
-                .height * 0.05,),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
             FutureBuilder(
               future: responssGetApi.get_all(url: ApiData.all_country_data_api),
               builder: (context, AsyncSnapshot<Allapidata> snapshot) {
@@ -61,30 +53,39 @@ class _HomeScreenState extends State<HomeScreen> {
                   return Column(
                     children: [
                       Card(
-                        elevation: 10,
+                        elevation: 0,
+                        color: Colors.white,
                         margin: EdgeInsets.all(8),
                         child: Column(
                           children: [
-                            ListTile(title: Text("Action", style: TextStyle(
-                                fontSize: 20, color: Colors.red)),
+                            ListTile(
+                              title: Text("Action",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.red)),
                               trailing: Text("${snapshot.data!.active}",
                                   style: TextStyle(
                                       fontSize: 20, color: Colors.red)),
                             ),
-                            ListTile(title: Text("cases", style: TextStyle(
-                                fontSize: 20, color: Colors.red)),
+                            ListTile(
+                              title: Text("cases",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.red)),
                               trailing: Text("${snapshot.data!.cases}",
                                   style: TextStyle(
                                       fontSize: 20, color: Colors.red)),
                             ),
-                            ListTile(title: Text("tests", style: TextStyle(
-                                fontSize: 20, color: Colors.red)),
+                            ListTile(
+                              title: Text("tests",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.red)),
                               trailing: Text("${snapshot.data!.tests}",
                                   style: TextStyle(
                                       fontSize: 20, color: Colors.red)),
                             ),
-                            ListTile(title: Text("deaths", style: TextStyle(
-                                fontSize: 20, color: Colors.red)),
+                            ListTile(
+                              title: Text("deaths",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.red)),
                               trailing: Text("${snapshot.data!.deaths}",
                                   style: TextStyle(
                                       fontSize: 20, color: Colors.red)),
@@ -94,15 +95,20 @@ class _HomeScreenState extends State<HomeScreen> {
                       )
                     ],
                   );
-                }
-                else {
+                } else {
                   return Center(child: CircularProgressIndicator());
                 }
-              },), ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return CountryCoronaPage();
-              },));
-            }, child: Text("Country Page"))
+              },
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return CountryCoronaPage();
+                    },
+                  ));
+                },
+                child: Text("Country Page"))
           ],
         ),
       ),
